9b78a0ce5b1c47a0a88e413942f8abf16291f01c
thomashacker <EdwardSchmuhl@web.de>
2021-11-30 01:24:49 +0100

    Prettify code

--- project/scripts/clausecat/clause_segmentation.py
+++ project/scripts/clausecat/clause_segmentation.py
@@ -27,13 +27,13 @@ class ClauseSegmentation:
         clauses = []
         split_indices = self.benepar_split(doc)
 
-        for index_pair in split_indices:
-            current_span = doc[index_pair[0] : index_pair[1]]
+        for index_start, index_end in split_indices:
+            current_span = doc[index_start : index_end]
             if len(current_span.ents) != 0:
                 for entity in current_span.ents:
                     clauses.append(
                         {
-                            "split_indices": index_pair,
+                            "split_indices": (index_start,index_end),
                             "has_ent": True,
                             "ent_indices": (entity.start, entity.end),
                             "blinder": f"_{entity.label_}_",
@@ -44,7 +44,7 @@ class ClauseSegmentation:
             else:
                 clauses.append(
                     {
-                        "split_indices": index_pair,
+                        "split_indices": (index_start,index_end),
                         "has_ent": False,
                         "ent_indices": (0, 0),
                         "blinder": None,
@@ -71,12 +71,12 @@ class ClauseSegmentation:
         for sentence in doc.sents:
             can_split = False
             for constituent in sentence._.constituents:
-                # Include every clause with the label "S" (Sentence) and with the root sentence as parent
+                # Store start/end indices of clauses labeled "S" (Sentence) if their parent is the original sentence
                 if "S" in constituent._.labels and constituent._.parent == sentence:
                     split_indices.append((constituent.start, constituent.end))
                     can_split = True
 
-            # If no clause found just append the whole sentence
+            # If no clause found, append the start/end indices of the whole sentence
             if not can_split:
                 split_indices.append((sentence.start, sentence.end))
 
